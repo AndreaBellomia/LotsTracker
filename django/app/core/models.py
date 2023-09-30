9df11cc2da1ef522f57d1ef95c055ef52d436133
Andrea Bellomia <119261787+AndreaBellomia@users.noreply.github.com>
2023-10-01 00:23:02 +0200

    model update

--- django/app/core/models.py
+++ django/app/core/models.py
@@ -1,9 +1,8 @@
-from enum import unique
-import re
-from tabnanny import verbose
+from typing import Any
 from django.db import models
 
-# Create your models here.
+from django.db.models import Case, When, Value, CharField, Count, QuerySet
+from django.http import QueryDict
 
 
 class AbstractBaseModel(models.Model):
@@ -55,7 +54,11 @@ class DocumentFromSupplier(AbstractBaseModel):
     Registry of all documents from a supplier
     """
 
-    supplier = models.ForeignKey(SupplierRegistry, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(
+        SupplierRegistry,
+        on_delete=models.CASCADE,
+        related_name="document_from_supplier",
+    )
     date = models.DateField()
     number = models.CharField(max_length=50, db_index=True)
 
@@ -74,7 +77,9 @@ class DocumentToSupplier(AbstractBaseModel):
     Registry of all documents to a supplier
     """
 
-    supplier = models.ForeignKey(SupplierRegistry, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(
+        SupplierRegistry, on_delete=models.CASCADE, related_name="document_to_supplier"
+    )
     date = models.DateField()
     number = models.CharField(max_length=50, db_index=True)
 
@@ -100,6 +105,7 @@ class DocumentCustomer(AbstractBaseModel):
         null=True,
         default=None,
         db_index=True,
+        related_name="document_customer",
     )
     date = models.DateField(db_index=True)
     number = models.CharField(max_length=50, db_index=True)
@@ -109,6 +115,41 @@ class DocumentCustomer(AbstractBaseModel):
     def save(self, *args, **kwargs):
         self.year = self.date.year
         super().save(*args, **kwargs)
+
+    def get_status(self):
+        status = self.warehouse_items.aggregate(
+            open_count=Count(
+                Case(
+                    When(
+                        status=WarehouseItems.WarehouseItemsStatus.BOOKED, then=Value(1)
+                    ),
+                    default=None,
+                )
+            ),
+            closed_count=Count(
+                Case(
+                    When(
+                        status=WarehouseItems.WarehouseItemsStatus.EMPTY, then=Value(1)
+                    ),
+                    When(
+                        status=WarehouseItems.WarehouseItemsStatus.RETURNED,
+                        then=Value(1),
+                    ),
+                    default=None,
+                )
+            ),
+            total_count=Count("id"),
+        )
+
+        if status["total_count"] > 0:
+            if status["open_count"] and not status["closed_count"]:
+                return "Open"
+            elif status["closed_count"] and not status["open_count"]:
+                return "Closed"
+            else:
+                return "Partial"
+        else:
+            return "Empty"
 
     class Meta:
         unique_together = (("year"), ("number"))
@@ -143,13 +184,13 @@ class WarehouseItems(AbstractBaseModel):
     empty_date = models.DateField(null=True, blank=True, db_index=True)
     batch_code = models.CharField(max_length=100, db_index=True)
     item_type = models.ForeignKey(
-        WarehouseItemsRegistry, on_delete=models.CASCADE, related_name="items_type"
+        WarehouseItemsRegistry, on_delete=models.CASCADE, related_name="warehouse_items"
     )
 
     document_from_supplier = models.ForeignKey(
         DocumentFromSupplier,
         on_delete=models.SET_NULL,
-        related_name="document_from_supplier",
+        related_name="warehouse_items",
         null=True,
         blank=True,
         default=None,
@@ -157,7 +198,7 @@ class WarehouseItems(AbstractBaseModel):
     document_to_supplier = models.ForeignKey(
         DocumentToSupplier,
         on_delete=models.SET_NULL,
-        related_name="document_to_supplier",
+        related_name="warehouse_items",
         null=True,
         blank=True,
         default=None,
@@ -165,7 +206,7 @@ class WarehouseItems(AbstractBaseModel):
     document_customer = models.ForeignKey(
         DocumentCustomer,
         on_delete=models.CASCADE,
-        related_name="document_customer",
+        related_name="warehouse_items",
         null=True,
         blank=True,
         default=None,
@@ -187,18 +228,40 @@ class WarehouseItems(AbstractBaseModel):
         blank=True,
     )
 
+    def _get_current_status(self):
+        if self.document_to_supplier:
+            return self.WarehouseItemsStatus.RETURNED
+        elif self.empty_date:
+            return self.WarehouseItemsStatus.EMPTY
+        elif self.document_customer:
+            return self.WarehouseItemsStatus.BOOKED
+        elif self.document_from_supplier:
+            return self.WarehouseItemsStatus.AVAILABLE
+        else:
+            return self.WarehouseItemsStatus.AVAILABLE
+
+    def _get_auto_update_of_status(self):
+        return (
+            self._empty_date != self.empty_date
+            or self._document_from_supplier != self.document_from_supplier
+            or self._document_to_supplier != self.document_to_supplier
+            or self._document_customer != self.document_customer
+        )
+
     def save(self, *args, **kwargs):
         _status = None
-        if self.custom_status:
+        if self.custom_status and not self._get_auto_update_of_status():
             _status = self.custom_status
         else:
-            if self.document_to_supplier:
-                _status = self.WarehouseItemsStatus.RETURNED
-            elif self.empty_date:
-                _status = self.WarehouseItemsStatus.EMPTY
-            elif self.document_customer:
-                _status = self.WarehouseItemsStatus.BOOKED
-            elif self.document_from_supplier:
-                _status = self.WarehouseItemsStatus.AVAILABLE
+            self.custom_status = None
+            _status = self._get_current_status()
         self.status = _status
         super().save(*args, **kwargs)
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self._empty_date = self.empty_date
+        self._document_from_supplier = self.document_from_supplier
+        self._document_to_supplier = self.document_to_supplier
+        self._document_customer = self.document_customer
+        self._custom_status = self.custom_status
