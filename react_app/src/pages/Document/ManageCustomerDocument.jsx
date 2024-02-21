7de176cdb22217853ea65ea80e5ec0261f8db948
Andrea Bellomia <119261787+AndreaBellomia@users.noreply.github.com>
2024-02-21 11:33:31 +0100

    complete form

--- react_app/src/pages/Document/ManageCustomerDocument.jsx
+++ react_app/src/pages/Document/ManageCustomerDocument.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import dayjs from "dayjs";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
     FormControl,
     Grid,
@@ -10,70 +10,49 @@ import {
     Button,
     Box,
     Typography,
-    Divider,
     Paper,
     IconButton,
-    MenuItem,
     TableContainer,
     TableHead,
     TableRow,
     Table,
     TableBody,
-    TableCell
+    TableCell,
 } from "@mui/material";
 import { DatePicker, ButtonDocumentBig } from "../../layout/components";
 import { manageHandlerInput } from "../../libs/forms.js";
 
-import { RemoveCircle, AddCircle, Edit, Done } from '@mui/icons-material';
+import { RemoveCircle, AddCircle, Edit, Done } from "@mui/icons-material";
 
 import FetchApi, { manageFetchError } from "../../libs/axios.js";
 
+import SelectItemModal from "../../components/Modals/SelectItem.jsx";
+import SelectCustomerModal from "../../components/Modals/SelectCustomerModal.jsx";
 
-import SelectItemModal from "../../components/Modals/SelectItem.jsx"
-import SelectCustomerModal from "../../components/Modals/SelectCustomerModal.jsx"
-
-import { useSnackbar } from 'notistack';
-
-
-class BodyItem {
-    constructor(id, description, itemCode, batchCode) {
-        this.id = id;
-        this.description = description, 
-        this.itemCode = itemCode,
-        this.batchCode = batchCode;
-
-    }
-
-    getItemBody() {
-        return { id: this.id }
-    }
-}
+import { useSnackbar } from "notistack";
 
 
 class ManageFormBodies {
     constructor(body, setBody, itemCode, batchCode) {
-        this.body = body
-        this.setBody = setBody
+        this.body = body;
+        this.setBody = setBody;
     }
 
     append(item) {
-        if (!this.body.some(existingItem => existingItem.id === item.id)) {
+        if (!this.body.some((existingItem) => existingItem.id === item.id)) {
             this.setBody([...this.body, item]);
         }
     }
 
     remove(id) {
-        const updatedValues = this.body.filter(item => item.id !== id);
+        const updatedValues = this.body.filter((item) => item.id !== id);
         this.setBody(updatedValues);
     }
 }
 
-
-
 function getFormBody(formBody) {
-    return formBody.map(item => Object({ "id" : item.id }))
+    return formBody.map((item) => Object({ id: item.id }));
 }
-
 
 export default function ManageDocument() {
     const navigate = useNavigate();
@@ -94,53 +73,53 @@ export default function ManageDocument() {
     const HandlerInput = new manageHandlerInput(formValues, setFormValues, setFormErrors);
     const formBodies = new ManageFormBodies(formValuesBody, setFormValuesBody);
 
-    useEffect(()=> {
+    useEffect(() => {
         setFormValues({
-           ...formValues,
-           body: getFormBody(formValuesBody),
-        })
+            ...formValues,
+            body: getFormBody(formValuesBody),
+        });
     }, [formValuesBody]);
-    
-    useEffect(()=> {
+
+    useEffect(() => {
         setFormValues({
-           ...formValues,
-           customer_id: formValuesCustomer.id,
-        })
+            ...formValues,
+            customer_id: formValuesCustomer.id,
+        });
     }, [formValuesCustomer]);
 
-
     const POSTapi = () => {
-        console.log(formValues)
+        console.log(formValues);
         try {
             new FetchApi()
                 .postCustomerDocument(formValues)
                 .then((res) => {
                     navigate("/documenti");
+                    handleClickVariant("Documento creato correttamente", "success");
                 })
                 .catch((error) => {
-                    console.log(error)
-                    const variant = "error"
-                    enqueueSnackbar(error.response.data.detail || "Error durante la creazione del documento!" , { variant });
+                    handleClickVariant(
+                        error.response.data.detail || "Error durante la creazione del documento!",
+                        "error"
+                    );
                     if (!error.status === 400) {
                         throw new Error("Error during request: " + error);
                     }
                     if (!error.status === undefined) {
                         navigate("/documenti");
                     }
-                    console.log(error)
-                    
                     manageFetchError(error, formErrors, setFormErrors);
                 });
         } catch (error) {
-           
             console.error(error);
         }
     };
 
+    const handleClickVariant = (msg, variant) => () => {
+        enqueueSnackbar(msg, { variant });
+    };
 
     return (
-        <>  
-            
+        <>
             <Typography variant="h4">Documento di consegna</Typography>
 
             <Box mt={4} />
@@ -152,7 +131,7 @@ export default function ManageDocument() {
                         direction="row"
                         sx={{
                             p: 2,
-                            border: formErrors.item_type_id ? "1px solid #FF5630" : "none",
+                            border: formErrors.customer_id ? "1px solid #FF5630" : "none",
                             height: "100%",
                             display: "flex",
                             flexDirection: "column",
@@ -187,7 +166,7 @@ export default function ManageDocument() {
                 </Grid>
                 <Grid item xs={12} md={6} lg={6}>
                     <Box>
-                        <FormControl sx={{ width: "100%" }}>   
+                        <FormControl sx={{ width: "100%" }}>
                             <FormLabel>Data documento</FormLabel>
                             <DatePicker
                                 onChange={(target) => {
@@ -198,12 +177,12 @@ export default function ManageDocument() {
                                 value={formValues.date ? dayjs(formValues.date) : null}
                             />
                         </FormControl>
-                        <FormControl sx={{ width: "100%", mt: 2 }}>   
+                        <FormControl sx={{ width: "100%", mt: 2 }}>
                             <FormLabel>Numero documento</FormLabel>
                             <TextField
                                 onChange={HandlerInput.handleInputChange}
                                 name="number"
-                                value={formValues.number || ''}
+                                value={formValues.number || ""}
                                 helperText={formErrors.number}
                                 error={Boolean(formErrors.number)}
                             ></TextField>
@@ -214,33 +193,48 @@ export default function ManageDocument() {
 
             <Box mt={4} />
 
-            <TableContainer component={Paper} >
+            <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
-                    <TableRow>
-                        <TableCell>Descrizione</TableCell>
-                        <TableCell align="right">Codice</TableCell>
-                        <TableCell align="right">Lotto</TableCell>
-                        <TableCell align="right"></TableCell>
-                    </TableRow>
+                        <TableRow>
+                            <TableCell>Descrizione</TableCell>
+                            <TableCell align="right">Codice</TableCell>
+                            <TableCell align="right">Lotto</TableCell>
+                            <TableCell align="right"></TableCell>
+                        </TableRow>
                     </TableHead>
                     <TableBody>
-                    {formValuesBody.map((row, index) => (
+                        {formValuesBody.map((row, index) => (
+                            <TableRow
+                                key={index}
+                                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.item_type.description}
+                                </TableCell>
+                                <TableCell align="right">{row.item_type.internal_code}</TableCell>
+                                <TableCell align="right">{row.batch_code}</TableCell>
+                                <TableCell align="right">
+                                    <IconButton onClick={() => formBodies.remove(row.id)}>
+                                        <RemoveCircle color="error" />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
                         <TableRow
-                        key={index}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            sx={{
+                                "&:last-child td, &:last-child th": {
+                                    border: 0,
+                                    textAlign: "center",
+                                },
+                            }}
                         >
-                        <TableCell component="th" scope="row">
-                            {row.item_type.description}
-                        </TableCell>
-                        <TableCell align="right">{row.item_type.internal_code}</TableCell>
-                        <TableCell align="right">{row.batch_code}</TableCell>
-                        <TableCell align="right"><IconButton onClick={() => formBodies.remove(row.id)}><RemoveCircle color="error"/></IconButton></TableCell>
+                            <TableCell colSpan={4} align="right">
+                                <IconButton onClick={() => setItemModal(true)}>
+                                    <AddCircle color="secondary" />
+                                </IconButton>
+                            </TableCell>
                         </TableRow>
-                    ))}
-                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0, textAlign: "center" } }}>
-                        <TableCell colSpan={4} align="right"><IconButton onClick={() => setItemModal(true)}><AddCircle color="secondary"/></IconButton></TableCell>
-                    </TableRow>
                     </TableBody>
                 </Table>
             </TableContainer>
@@ -254,7 +248,7 @@ export default function ManageDocument() {
                         Annulla
                     </Button>
                 </Link>
-                
+
                 <Button variant="contained" size="medium" color="primary" onClick={() => POSTapi()}>
                     <Done />
                     <Box mr={1} />
@@ -262,8 +256,14 @@ export default function ManageDocument() {
                 </Button>
             </Box>
 
-           <SelectItemModal modalState={[itemModal, setItemModal]} tableChoices={(item) => formBodies.append(item)}/>
-           <SelectCustomerModal modalState={[customerModal, setCustomerModal]} tableChoices={(item) => setFormValuesCustomer(item)}/>
+            <SelectItemModal
+                modalState={[itemModal, setItemModal]}
+                tableChoices={(item) => formBodies.append(item)}
+            />
+            <SelectCustomerModal
+                modalState={[customerModal, setCustomerModal]}
+                tableChoices={(item) => setFormValuesCustomer(item)}
+            />
         </>
     );
 }
